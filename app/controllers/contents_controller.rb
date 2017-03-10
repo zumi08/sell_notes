501918da441504c07cd967b7a2390f216ae493c4
森住康平 <zumi.k081912@gmail.com>
2017-03-10 17:27:35 +0900

    部分テンプレートに切り出し、リファクタリング等

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,7 +3,6 @@ class ContentsController < ApplicationController
   layout 'af_log'
   def content
     if params[:some].present? && lookup_context.exists?("#{params[:some]}", "contents")
-      binding.pry
       return render action: "#{params[:some]}", layout: 'af_log'
     elsif params[:certain].present? && lookup_context.exists?("#{params[:certain]}", "contents/#{params[:symbol]}")
       return render action: "#{params[:symbol]}/#{params[:certain]}", layout: 'qualification'
