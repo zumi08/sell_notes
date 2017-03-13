c55d455bc3f5bf13a44c48d6a5410d61dd7180d8
森住康平 <zumi.k081912@gmail.com>
2017-03-13 13:46:37 +0900

    ログイン後top再修正、登録フォームの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, :except => ["destroy"]
   def after_sign_in_path_for(resource)
-    '/items'
+    '/top'
   end
 
   def after_sign_out_path_for(resource)
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
   private
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << [:name,:birthday,:adress,:univ,:colledge,:remenber_me]
+      devise_parameter_sanitizer.for(:sign_up) << [:name,:birthday,:adress,:college_id,:remenber_me]
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :birthday, :adress, :univ, :college,:password,:password_confirmation)
+      params.require(:user).permit(:name, :email, :birthday, :adress, :college_id,:password,:password_confirmation)
     end
 end
