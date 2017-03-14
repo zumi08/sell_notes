c20c800b0cc7987fd1baa8d5ec7cb9fa2e05e2cb
森住康平 <zumi.k081912@gmail.com>
2017-03-14 20:10:37 +0900

    item投稿ページ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -75,7 +75,7 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:title,:college_id, :detail, :price, :file,:user_id)
+      params.require(:item).permit(:title,:college_id, :detail, :price, :file,:user_id,:year,:hand_over_way)
     end
 
     def uploadpdf(file_object,file_name)
